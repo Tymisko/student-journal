b4a97c185ace8775c3087ad70cb6f5ca94fa6a57
Jan Urbaś <janurbas39@gmail.com>
2022-04-16 13:59:59 +0200

    Implement Students property of generic ObservableCollection type and SelectedStudent. Disable automatically generate rows.

--- student-journal/ViewModels/MainViewModel.cs
+++ student-journal/ViewModels/MainViewModel.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Input;
 using Diary.Commands;
+using Diary.Models;
 
 namespace Diary.ViewModels
 {
@@ -14,8 +16,56 @@ namespace Diary.ViewModels
         public MainViewModel()
         {
             RefreshStudentsCommand = new RelayCommand(RefreshStudents, CanRefreshStudents);
+            Students = new ObservableCollection<Student>()
+            {
+                new Student
+                {
+                    FirstName = "Patryk", 
+                    LastName = "Matczak", 
+                    Group = new Group { Id = 0 }
+                },
+
+                new Student
+                {
+                    FirstName = "Kamil",
+                    LastName = "Nowak",
+                    Group = new Group { Id = 0 }
+                },
+
+                new Student
+                {
+                    FirstName = "Janusz",
+                    LastName = "Kaczyński",
+                    Group = new Group { Id = 0 }
+                },
+            };
         }
         public ICommand RefreshStudentsCommand { get; set; }
+
+        private Student _selectedStudent;
+
+        public Student SelectedStudent
+        {
+            get => _selectedStudent;
+            set
+            {
+                _selectedStudent = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private ObservableCollection<Student> _students;
+
+        public ObservableCollection<Student> Students
+        {
+            get => _students;
+            set
+            {
+                _students = value;
+                OnPropertyChanged();
+            }
+        }
+
 
         private bool CanRefreshStudents(object obj)
         {
