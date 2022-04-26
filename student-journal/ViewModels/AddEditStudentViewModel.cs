a92e9eaa581260883b56746f0c77a376fd43284e
Jan Urbaś <janurbas39@gmail.com>
2022-04-26 06:01:12 +0200

    Create ctor for AddEditStudentViewmodel

--- student-journal/ViewModels/AddEditStudentViewModel.cs
+++ student-journal/ViewModels/AddEditStudentViewModel.cs
@@ -3,10 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Diary.Models;
 
 namespace Diary.ViewModels
 {
     internal class AddEditStudentViewModel : ViewModelBase
     {
+        public AddEditStudentViewModel(Student student = null)
+        {
+            
+        }
     }
 }
