6a6f2eac0cb6638a54064245b3849585e244a179
Jan Urbaś <janurbas39@gmail.com>
2022-04-29 08:14:51 +0200

    Include configuration files in AppplicationDbContext

--- student-journal/ApplicationDbContext.cs
+++ student-journal/ApplicationDbContext.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Data.Entity;
 using System.Linq;
+using Diary.Models.Configurations;
+using Diary.Models.Domains;
 
 namespace Diary
 {
@@ -10,6 +12,17 @@ namespace Diary
             : base("name=ApplicationDbContext")
         {
 
+        }
+
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Group> Groups { get; set; }
+        public DbSet<Rating> Ratings { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Configurations.Add(new StudentConfiguration());
+            modelBuilder.Configurations.Add(new GroupConfiguration());
+            modelBuilder.Configurations.Add(new RatingConfiguration());
         }
     }
 }
