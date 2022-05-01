11e67bc0b77dd558f5db0230150e36e0c884ff50
Jan Urbaś <janurbas39@gmail.com>
2022-05-01 10:51:49 +0200

    Remove Ratings Collection that was added by misstake

--- student-journal/Models/Domains/Rating.cs
+++ student-journal/Models/Domains/Rating.cs
@@ -8,19 +8,11 @@ using System.Threading.Tasks;
 namespace Diary.Models.Domains
 {
     public class Rating
-    {
-        public Rating()
-        {
-            Ratings = new Collection<Rating>();
-        }
-
+    { 
         public int Id { get; set; }
         public int Rate { get; set; }
         public int StudentId { get; set; }
         public int SubjectId { get; set; }
-
         public Student Student { get; set; }
-
-        public ICollection<Rating> Ratings { get; set; }
     }
 }
--- student-journal/Models/Domains/Student.cs
+++ student-journal/Models/Domains/Student.cs
@@ -1,9 +1,15 @@
 ﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
 
 namespace Diary.Models.Domains
 {
     public class Student
     {
+        public Student()
+        {
+            Ratings = new Collection<Rating>();
+        }
+
         public int Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
