25bc7cc7b80d435df29e25e5b054ee2c00905138
Jan Urbaś <janurbas39@gmail.com>
2022-05-03 10:15:57 +0200

    Add group data validation in AddEditViewModel

--- student-journal/Models/Wrappers/GroupWrapper.cs
+++ student-journal/Models/Wrappers/GroupWrapper.cs
@@ -1,8 +1,30 @@
-﻿namespace Diary.Models.Wrappers
+﻿using System.ComponentModel;
+
+namespace Diary.Models.Wrappers
 {
-    public class GroupWrapper
+    public class GroupWrapper : IDataErrorInfo
     {
         public int Id { get; set; }
         public string Name { get; set; }
+
+        public string this[string columnName]
+        {
+            get
+            {
+                switch (columnName)
+                {
+                    case nameof(Id):
+                        Error = Id == 0 ? "Group field is required!" : string.Empty;
+                        break;
+
+                    default:
+                        break;
+                }    
+
+                return Error;
+            }
+        }
+
+        public string Error { get; set; }
     }
 }
